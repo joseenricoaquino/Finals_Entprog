09912a3c6345355d56ae5fe700f08b20da4bef07
Jose Enrico Aquino <12020378@benilde.edu.ph>
2021-10-26 20:38:09 +0800

    Unwind project created - jose

---
+++ Entprog_Finals_Unwind/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Entprog_Finals_Unwind.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
